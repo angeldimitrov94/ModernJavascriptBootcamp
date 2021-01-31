ad19505b39d24216896e62348677796fa23eafd1
Angel Dimitrov <angel.dimitrov@omron.com>
2021-01-31 12:22:24 -0800

    Remove extra files that I dont want in the repo

--- Part 1/Objects/objects.js
+++
@@ -1,9 +0,0 @@
-const aTestObject = {
-    propertyOne : 12,
-    propertyTwo : "Angel",
-    propertyThree : "abcdefghijklmnopqrstuvxyz"
-}
-
-for (const property in aTestObject) {
-    console.log(`${property}: ${aTestObject[property]}`);
-}
